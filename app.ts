bdbd3f824ec83b69689858e0ea91f793927e109a
gameover2008 <gameover2008@outlook.com>
2022-10-21 18:45:42 +0200

    neg works

--- app.ts
+++ app.ts
@@ -2,18 +2,23 @@ import { argv } from 'process'
 import {
   xml2json,
   prependsec,
-  saveJsontoXml
+  saveJsontoXml,
+  removesec
 } from './f'
 
 const [inputfile, nSec, outputfile] = argv.slice(2)
 
 console.log('---moveit---')
 
-const prependCall = (n: number) => (j) => prependsec(j, n)
+// const prependCall = (n: number) => (j) => prependsec(j, n)
+// const removesecCall = (n: number) => (j) => removesec(j, n)
+
+const callSelector = (n: number) => (j) => n > 0 ? prependsec(j, n) : removesec(j, -1 * n)
+
 const saveJsontoXmlCall = (fn: string) => (data) => saveJsontoXml(data, fn)
 
 xml2json(inputfile)
-  .then(prependCall(parseInt(nSec)))
+  .then(callSelector(parseInt(nSec)))
   .then(saveJsontoXmlCall(outputfile))
   .then(() => { console.log('completed!') })
   .catch(console.log)
