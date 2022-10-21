5e7892018f084aeaad793bb7917e32928645e497
Mats Andersson <mats.andersson@cgi.com>
2022-10-21 13:19:45 +0200

    debugprobl

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
+import { argv } from 'process'
+import {
+  xml2json,
+  prepend,
+  saveJsontoXml
+} from 'f'
+
+const [inputfile, nSec, outputfile] = argv
+
 console.log('moveit');
+
+const prependCall = (n) => (j) => prepend(j, n)
+const saveJsontoXmlCall = (fn) => (data) => saveJsontoXml(data, fn)
+
+xml2json(inputfile)
+  .then(prependCall(nSec))
+  .then(saveJsontoXmlCall(outputfile))
+  .then(() => { console.log('completed!') })
+  .catch(console.log)
